6957c4f23de3ca0c8db7fcda0ddaba201d747839
Chris McMeeking <chriscm2006@gmail.com>
2023-12-28 01:33:00 -0500

    Sending and receiving images!

---
+++ N-ARIA-UITests/N_ARIA_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  N_ARIA_UITestsLaunchTests.swift
+//  N-ARIA-UITests
+//
+//  Created by Chris McMeeking on 12/26/23.
+//
+
+import XCTest
+
+final class N_ARIA_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
