6a016a89244827a212c29bb37a843297e4aacc91
Chris McMeeking <chriscm2006@gmail.com>
2024-01-18 04:43:25 -0500

    Remove Accessibility Guidelines section. Keep this app simple.

--- App.swift
+++ App.swift
@@ -28,39 +28,133 @@ struct MainContent: View {
 		
 		NAriaApp(title: "N-ARIA") {
 			
-			VStack {
-				ScrollView {
+			NAriaList {
+				
+				NAriaSection(header: "Components") {
 					
-					NAriaBox {
-						VStack {
-							NAriaHeadline(text:"Learn")
-							NAriaSubTitle(text:"Presents select N-ARIA Design System Components and explains the related Accessibility Principles.")
-							NAriaCTA(label: "iOS Accessibility Guidelines") {
-								Nav.shared.navigateTo(.guidelines)
-							}.padding().accessibilityIdentifier("guidelines")
-						}
-					}.padding()
+					NAriaNavigationLink {
+						NAriaListIcon(image: "person.text.rectangle", label: "NAriaContactCard")
+					} destination: {
+						NAriaContactCard(contact: Data.contacts.first!).navigationTitle("NAriaContactCard")
+					}
 					
-					NAriaBox {
-						
-						VStack {
-							NAriaHeadline(text:"Explore")
-							NAriaSubTitle(text:"The entire N-ARIA Design System by component. Link your annotation kits to these articles on our documentation site!")
-							NAriaCTA(label: "N-ARIA Design System") {
-								Nav.shared.navigateTo(.patterns)
-							}.padding().accessibilityIdentifier("design")
+					NAriaNavigationLink {
+						NAriaListIcon(image: "button.horizontal", label: "NAriaCTA")
+					} destination: {
+						NAriaCTA(label: "ClickMe") { }.navigationTitle("NAriaCTA")
+					}
+					
+					NAriaNavigationLink {
+						NAriaListIcon(image: "button.programmable.square", label: "NAriaIcon")
+					} destination: {
+						NAriaIcon {
+							Image(systemName: "person")
+						} label: {
+							Text("Person")
+						}.navigationTitle("NAriaIcon")
+					}
+					
+					NAriaNavigationLink {
+						NAriaListIcon(image: "list.dash", label: "NAriaList")
+					} destination: {
+						NAriaList {
+							NAriaSection(header: "A Section") {
+								NAriaListIcon(image: "Icon-TextField", label: "Text Field")
+							}
+						}.navigationTitle("NAriaList")
+					}
+					
+					NAriaNavigationLink {
+						NAriaListIcon(image: "text.line.first.and.arrowtriangle.forward", label: "NAriaListItem")
+					} destination: {
+						NAriaListIcon(image: "character.textbox", label: "NAriaList").navigationTitle("NAriaListItem")
+					}
+					
+					NAriaNavigationLink {
+						NAriaListIcon(image: "person.crop.circle", label: "NAriaProfile")
+					} destination: {
+						NAriaProfile(contact: Data.contacts.first!).navigationTitle("NAriaProfile")
+					}
+					
+					NAriaNavigationLink {
+						NAriaListIcon(image: "text.line.last.and.arrowtriangle.forward", label: "NAriaSection")
+					} destination: {
+						NAriaList {
+							NAriaSection(header: "A Section") {
+								NAriaListIcon(image: "Icon-TextField", label: "Text Field")
+							}
+							
+							NAriaSection(header: "Another Section") {
+								NAriaListIcon(image: "Icon-TextField", label: "Text Field")
+							}
+						}.navigationTitle("NAriaSection")
+					}
+					
+					NAriaNavigationLink {
+						NAriaListIcon(image: "character.textbox", label: "NAriaTextField")
+					} destination: {
+						NAriaTextField(label: "Name").navigationTitle("NAriaTextField")
+					}
+					
+					NAriaNavigationLink {
+						NAriaListIcon(image: "bold", label: "NAriaHeadline")
+					} destination: {
+						NAriaHeadline(text: "N-ARIA Rocks").navigationTitle("NAriaHeadline")
+					}
+					
+					NAriaNavigationLink {
+						NAriaListIcon(image: "text.append", label: "NAriaSubtitle")
+					} destination: {
+						NAriaSubTitle(text: "The N-ARIA Documentation Engine is cool.").navigationTitle("NAriaSubtitle")
+					}
+					
+					NAriaNavigationLink {
+						NAriaListIcon(image: "square.on.square.squareshape.controlhandles", label: "NAriaBox")
+					} destination: {
+						NAriaBox {
+							VStack {
+								NAriaHeadline(text: "N-ARIA Rocks").navigationTitle("NAriaHeadline")
+								NAriaHeadline(text: "The N-ARIA Documentation Engine is cool.").navigationTitle("NAriaBox")
+							}
 						}
-					}.padding()
+					}
 				}
 				
-				Spacer()
-								
-				NAriaUser(user:$user)
-									
-			}.navigationDestination(for:Collections.self) { destination in
-				Collections.setViewForDestination(destination)
+				NAriaSection(header: Strings.demosHeader()) {
+					NAriaNavigationLink {
+						NAriaListIcon(image: "person.3", label: "NAriaContacts")
+					} destination: {
+						NAriaContacts(list:Data.contacts)
+					}
+					
+					NAriaNavigationLink {
+						NAriaListIcon(image: "dots.and.line.vertical.and.cursorarrow.rectangle", label: "NAriaMasterDetail")
+					} destination: {
+						NAriaMasterDetail {
+							VStack {
+								NAriaBox {
+									VStack {
+										NAriaHeadline(text: "A Summary")
+										NAriaSubTitle(text: "Maybe a subtitle.")
+									}
+								}
+							}
+						} detail: {
+							VStack {
+								NAriaHeadline(text: "A Summary")
+								NAriaSubTitle(text: "Maybe a subtitle.")
+								NAriaSubTitle(text: "And now ramble as much as you want about the topic! The user asked for these details. They want them! :)")
+							}
+						}
+					}
+					
+					NAriaNavigationLink {
+						NAriaListIcon(image: "person", label: "NAriaPerson")
+					} destination: {
+						NAriaPerson().navigationTitle("NAriaPerson")
+					}
+				}
 			}
-			
 		}.environmentObject(Nav.shared)
 			.environmentObject(user)
 	}
